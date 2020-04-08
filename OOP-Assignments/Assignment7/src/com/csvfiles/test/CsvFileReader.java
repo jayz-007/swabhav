a4bc5455cc8a87b297126405b9bda404e6ceb287
jay <jayshelke84@gmail.com>
2020-04-08 23:48:02 +0530

    apr08/completed(1)

--- OOP-Assignments/Assignment7/src/com/csvfiles/test/CsvFileReader.java
+++ OOP-Assignments/Assignment7/src/com/csvfiles/test/CsvFileReader.java
@@ -9,12 +9,12 @@ public class CsvFileReader {
 	private static final String String = null;
 
 	public static void main(String args[]) throws IOException {
-
+		int totalrows = 10;
 		String[][] products = new String[10][4];
 		
-		int totalrows = loadCsvFile(products);
-		swapColumns(products, totalrows);
-		printCsv(products, totalrows);
+		int totalcolumns = loadCsvFile(products);
+		swapColumns(products,totalrows,totalcolumns);
+		printCsv(products,totalrows, totalcolumns);
 
 	}
 
@@ -22,35 +22,47 @@ public class CsvFileReader {
 		String line = "";
 		String splitBy = ",";
 		int rows = 0;
+		String[] product = null;
 		BufferedReader br = new BufferedReader(new FileReader("products.csv"));
-		while ((line = br.readLine()) != null) // returns a Boolean value
+		while ((line = br.readLine()) != null) 
 		{
-			String[] product = line.split(splitBy);
+			product = line.split(splitBy);
 			for (int j = 0; j < product.length; j++) {
 				products[rows][j] = String.valueOf(product[j]);
 			}
 			rows++;
 
 		}
-		return rows;
+		
+		return product.length ;
 	}
 
-	public static void swapColumns(String[][] products, int totalrows) {
+	public static void swapColumns(String[][] products, int totalrows , int totalcolumns) {
 		String temp = "";
+		int price = 0 , discount = 0;
+		for (int column = 0; column < totalcolumns; column++) {
+			if(products[0][column].contains("price")) {
+				 price = column;
+			}
+			else if(products[0][column].contains("discount")) {
+				 discount = column;
+			}
+		}
+		
 		for (int row = 0; row < totalrows; row++) {
 
-			temp = products[row][2];
-			products[row][2] = products[row][3];
-			products[row][3] = temp;
+			temp = products[row][price];
+			products[row][price] = products[row][discount];
+			products[row][discount] = temp;
 		}
 	}
 
-	public static void printCsv(String[][] products, int totalrows) {
+	public static void printCsv(String[][] products, int totalrows,int totalcolumns) {
 		for (int row = 0; row < totalrows; row++) {
 			if (products[row][1] != null) {
-				for (int j = 0; j <= 3; j++) {
-					if (products[row][j] != null)
-						System.out.print(products[row][j] + " ");
+				for (int column = 0; column < totalcolumns; column++) {
+					if (products[row][column] != null)
+						System.out.print(products[row][column] + " ");
 
 				}
 				System.out.println();
